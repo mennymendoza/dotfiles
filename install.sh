6ea681274617bc6a9171c8be878784db0dd81862
mennymendoza <juanmendoza19971@gmail.com>
2025-12-06 16:38:31 -0800

    feat: removed kitty.conf install

--- install.sh
+++ install.sh
@@ -6,6 +6,5 @@ DOTFILES_DIR="$(dirname "$(realpath "$0")")"
 # Overwrites file in home directory
 ln -sf "$DOTFILES_DIR/home/.tmux.conf" ~/.tmux.conf
 ln -sf "$DOTFILES_DIR/home/.bash_aliases" ~/.bash_aliases
-ln -sf "$DOTFILES_DIR/config/kitty/kitty.conf" ~/.config/kitty/kitty.conf
 ln -sf "$DOTFILES_DIR/config/lazygit/config.yml" ~/.config/lazygit/config.yml
 
