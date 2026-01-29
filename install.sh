f4be6c821d37ca6586b4e796014afd98c58b6bad
mennymendoza <juanmendoza19971@gmail.com>
2026-01-28 23:36:08 -0800

    feat: renamed files

--- install.sh
+++ install.sh
@@ -4,8 +4,8 @@
 DOTFILES_DIR="$(dirname "$(realpath "$0")")"
 
 # Creates symlinks
-ln -sf "$DOTFILES_DIR/home/.tmux.conf" ~/.tmux.conf
-ln -sf "$DOTFILES_DIR/home/.bash_aliases" ~/.bash_aliases
+ln -sf "$DOTFILES_DIR/home/tmux.conf" ~/.tmux.conf
+ln -sf "$DOTFILES_DIR/home/bash_aliases" ~/.bash_aliases
 ln -sf "$DOTFILES_DIR/config/lazygit/config.yml" ~/.config/lazygit/config.yml
 ln -sf "$DOTFILES_DIR/config/opencode/AGENTS.md" ~/.config/opencode/AGENTS.md
 ln -sf "$DOTFILES_DIR/config/opencode/opencode.json" ~/.config/opencode/opencode.json
