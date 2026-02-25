7f55464df609c06b803d69cd9648f6decb0b36d6
mennymendoza <juanmendoza19971@gmail.com>
2026-02-24 21:40:57 -0800

    feat: added nvim config

--- install.sh
+++ install.sh
@@ -9,4 +9,5 @@ ln -sf "$DOTFILES_DIR/home/bash_aliases" ~/.bash_aliases
 ln -sf "$DOTFILES_DIR/config/lazygit/config.yml" ~/.config/lazygit/config.yml
 ln -sf "$DOTFILES_DIR/config/opencode/AGENTS.md" ~/.config/opencode/AGENTS.md
 ln -sf "$DOTFILES_DIR/config/opencode/opencode.json" ~/.config/opencode/opencode.json
+ln -sf "$DOTFILES_DIR/config/nvim" ~/.config/nvim
 
