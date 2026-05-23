a70681caa32403f805cf07bc6c5cf033cf698842
mennymendoza <juanmendoza19971@gmail.com>
2026-05-23 13:37:31 -0700

    feat: updated treesitter config

--- config/nvim/lua/plugins/treesitter.lua
+++ config/nvim/lua/plugins/treesitter.lua
@@ -1,14 +1,15 @@
 return {
-    'nvim-treesitter/nvim-treesitter',
-    dependencies = { 'nvim-lua/plenary.nvim' },
-    lazy = false,
-    build = ':TSUpdate',
-    opts = {
-        ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc' },
-        auto_install = true,
-        highlight = {
-            enable = true,
-        },
-        indent = { enable = true },
-    },
+  'nvim-treesitter/nvim-treesitter',
+  branch = 'master',
+  dependencies = { 'nvim-lua/plenary.nvim' },
+  lazy = false,
+  build = ':TSUpdate',
+  config = function()
+    require('nvim-treesitter.configs').setup {
+      ensure_installed = { 'python', 'lua' },
+      auto_install = true,
+      highlight = { enable = true },
+      indent = { enable = true },
+    }
+  end,
 }
